aa3bb3747a9ac804d16f7b652b91ef3c93d147f2
knox <knox_01@DESKTOP-6R7UQS5.localdomain>
2025-08-19 14:48:50 -0400

    change root /, add readiness endpoint

--- main.go
+++ main.go
@@ -4,24 +4,55 @@ import (
 	"log"
 	"net/http"
 )
+// make a branch for this code
+
+// func main() {
+// 	const port = "8080"
+// 	const filepathRoot = "."
+
+// 	mux := http.NewServeMux()
+	
+// 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+//     w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+//     w.WriteHeader(http.StatusOK)
+//     w.Write([]byte("OK"))
+// 	})
+
+// 	// Serve index.html and other root files
+// 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+// 	// Serve files under /assets/ from ./assets/ directory
+//     mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+
+// 	srv := &http.Server{
+// 		Addr:    ":" + port,
+// 		Handler: mux,
+// 	}
+
+// 	log.Printf("Serving on port: %s\n", port)
+// 	log.Fatal(srv.ListenAndServe())
+// }
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-
-	// Serve index.html and other root files
-	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
-
-	// Serve files under /assets/ from ./assets/ directory
-    mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
