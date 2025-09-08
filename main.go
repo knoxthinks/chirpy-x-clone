331f8ecff5fdd683bea50f8c693df495b53afea8
knox <knox_01@DESKTOP-6R7UQS5.localdomain>
2025-09-08 10:41:22 -0400

    routing-stateful handlers

--- main.go
+++ main.go
@@ -1,46 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
-// make a branch for this code
 
-// func main() {
-// 	const port = "8080"
-// 	const filepathRoot = "."
-
-// 	mux := http.NewServeMux()
-	
-// 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-//     w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-//     w.WriteHeader(http.StatusOK)
-//     w.Write([]byte("OK"))
-// 	})
-
-// 	// Serve index.html and other root files
-// 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-
-// 	// Serve files under /assets/ from ./assets/ directory
-//     mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-
-
-// 	srv := &http.Server{
-// 		Addr:    ":" + port,
-// 		Handler: mux,
-// 	}
-
-// 	log.Printf("Serving on port: %s\n", port)
-// 	log.Fatal(srv.ListenAndServe())
-// }
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -51,8 +34,15 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
